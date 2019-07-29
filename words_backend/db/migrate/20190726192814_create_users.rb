2ee9ed773a255e22623d36ca9126c46f4726347d
JakubowskiA <arieljakubowski@gmail.com>
2019-07-29 11:40:46 -0400

    Edited schema

--- words_backend/db/migrate/20190726192814_create_users.rb
+++ words_backend/db/migrate/20190726192814_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :username
       t.string :password_digest
-      t.text :scores
+      t.integer :highscore
 
       t.timestamps
     end
--- words_backend/db/schema.rb
+++ words_backend/db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2019_07_26_193455) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
-    t.text "scores"
+    t.integer "highscore"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
