74b942f581e1f5ff26bb755bd10e095c4e655f00
JakubowskiA <arieljakubowski@gmail.com>
2019-07-31 17:22:33 -0400

    Revert "added leaderboard"
    This reverts commit 344a55482e5ce11ba8aaaa2d69f52f1cfa5cebc6.

--- words_backend/app/controllers/users_controller.rb
+++ words_backend/app/controllers/users_controller.rb
@@ -2,18 +2,18 @@ class UsersController < ApplicationController
   def index
     users = User.all.select { |user| user.highscore != nil }
     users = users.sort_by { |user| user.highscore }.reverse
-    render json: users, only: [:username, :highscore, :longest_word]
+    render json: users, only: [:username, :highscore]
   end
 
   def show
     user = User.find(params[:id])
-    render json: user, only: [:username, :longest_word]
+    render json: user, only: [:username]
   end
 
   def create
     # byebug
     user = User.find_or_create_by(username: params[:username])
-    render json: user, only: [:id, :username, :longest_word]
+    render json: user, only: [:id, :username]
   end
 
   def update
@@ -23,12 +23,7 @@ class UsersController < ApplicationController
     elsif (params[:highscore] > user[:highscore])
       user[:highscore] = params[:highscore]
     end
-    if (user.longest_word == nil)
-      user.longest_word = params[:longest_word]
-    elsif (user.longest_word.length < params[:longest_word].length)
-      user.longest_word = params[:longest_word]
-    end
     user.save
-    render json: user, only: [:id, :username, :highscore, :longest_word]
+    render json: user, only: [:id, :username, :highscore]
   end
 end
